c4392295fedeaaff29069c9443f40301af5b3e64
wagnercandido <wagnercssilva@gmail.com>
2019-06-11 10:42:22 -0300

    FlatList - organização de listas/objetos

--- App.js
+++ App.js
@@ -1,49 +1,53 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Platform, StyleSheet, Text, View, FlatList} from 'react-native';
+import { isTemplateElement } from '@babel/types';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      Nomes: [
+        {key: '1', nome: 'Wagner Candido', idade: 26},
+        {key: '2', nome: 'Arthur Filho', idade: 26},
+        {key: '3', nome: 'David Luna', idade: 26},
+        {key: '4', nome: 'Kaique Candido', idade: 26},
+        {key: '5', nome: 'Victor Medeiros', idade: 26},
+      ]
+    };
+  }
+
+  renderNome(item) {
+    return (
+      <View style={styles.viewNomes}>
+        <Text style={styles.nome}>{item.nome}</Text>
+        <Text style={styles.idade}>{item.idade}, anos</Text>
+      </View>
+    )
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        {/* <Text style={styles.welcome}>Welcome my first project</Text> */}
-        <View style={styles.header}>
-          <Text style={styles.title}>Primeiro App</Text>
-        </View>
-        <ScrollView>
-          <View style={styles.section1}>
-            <Text style={styles.texto}>Primeira Section</Text>
-          </View>
-          <View style={styles.section2}>
-            <Text style={styles.texto}>Segunda Section</Text>
-          </View>
-          <View style={styles.section1}>
-            <Text style={styles.texto}>Terceira Section</Text>
-          </View>
-          <View style={styles.section2}>
-            <Text style={styles.texto}>Quarta Section</Text>
-          </View>
-        </ScrollView>
+        <FlatList style={styles.flatList} data={this.state.Nomes} renderItem={({item}) => this.renderNome(item)}/>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  title: {
-    textAlign: 'center',
-    color: '#FFF'
+  flatList: {
+    height: 150
   },
-  section1: {
-    backgroundColor: '#0000ff',
-    height: 300,
+  viewNomes: {
+    height: 80,
+    padding: 10
   },
-  section2: {
-    backgroundColor: '#BBB',
-    height: 300,
+  nome: {
+    fontSize: 24,
   },
-  header: {
-    backgroundColor: 'red',
-    height: 50,
+  idade: {
+    fontSize: 16,
   }
+  
 });
