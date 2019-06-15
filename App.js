0a44455d518fde0dd230356b15399c2f29231797
wagnercandido <wagnercssilva@gmail.com>
2019-06-15 11:28:10 -0300

    Apresentacao Style/Props/State

--- App.js
+++ App.js
@@ -1,28 +1,61 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Switch } from 'react-native';
+import { Platform, StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+class MeuComponent extends Component {
+  render() {
+    return (
+      <Text>{this.props.propriedade}</Text>
+    );
+  }
+}
 
 export default class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      valor: false
+    this.state = { 
+
+      textState : 'Welcome my first project',
+      
     };
+
+    this.alteraTexto = this.alteraTexto.bind(this);
+
+  }
+
+  alteraTexto() {
+
+    this.state.textState = 'Texto Após clique';
+
+    this.setState(this.state);
   }
 
   render() {
-
-   
-
     return (
       <View style={styles.container}>
-        <Switch thumbTintColor="#0000FF" onTintColor="red" value={this.state.valor} onValueChange={(v) => this.setState({valor: v})} />
-        <Text>{(this.state.valor) ? "Selecionado" : "Não selecionado"}</Text>
+        <View style={styles.welcome}>
+
+          <MeuComponent propriedade={this.state.textState}></MeuComponent>
+          <Button title="sign in" onPress={this.alteraTexto} color="#4d58c1" />
+
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+  },
+  welcome: {
+    flex: 1,
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
   
 });
